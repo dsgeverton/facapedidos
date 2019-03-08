7c95aae0dfe32e897a3f884a8b3a9604771c290c
Everton Gonçalves <dsgeverton@gmail.com>
2019-03-08 17:49:46 -0300

    first commit

---
+++ src/main/java/br/com/codever/FacapedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.codever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacapedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacapedidosApplication.class, args);
+	}
+
+}
